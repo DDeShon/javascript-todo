e878b7a190066558fe0af2170f272449150f9b79
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-02 22:22:33 -0600

    working on list

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
         <div className="title-wrapper">
           <h1>Task Manager</h1>
         </div>
+        <div className="list-wrapper">
+          <ul className="todo">
+            <li>Go to the store</li>
+            <li>Go to the store</li>
+            <li>Go to the store</li>
+            <li>Go to the store</li>
+          </ul>
+        </div>
         <button className="btn-new-task">New Task</button>
       </div>
     </div>
