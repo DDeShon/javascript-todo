f6278c0a687e94aa369cdbb36e825ab1f30e68ab
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-11 08:45:02 -0600

    working on list functionality

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,3 +1,4 @@
+// Create a close button for each list item
 var todoList = document.getElementsByClassName("list-item");
 var i;
 for (i = 0; i < todoList.length; i++) {
@@ -7,3 +8,24 @@ for (i = 0; i < todoList.length; i++) {
   span.appendChild(txt);
   todoList[i].appendChild(span);
 }
+
+// Set up functionality to remove item upon clicking close
+var close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add check symbol upon clicking list item
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
