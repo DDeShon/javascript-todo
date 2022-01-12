11b2539d6c5116c4e58d7c271f72a52576109dc5
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-12 08:33:42 -0600

    working on list functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import TodoList from "./components/todoList";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
@@ -9,25 +10,12 @@ function App() {
           <h1>Task Manager</h1>
         </div>
         <div className="input-wrapper">
-          <input type="text" id="input" placeholder="Enter Task" />
-          <button className="btn-new-task">Add Task</button>
+          <TodoForm />
+          {/* <input type="text" id="input" placeholder="Enter Task" />
+          <button className="btn-new-task">Add Task</button> */}
         </div>
         <div className="list-wrapper">
           <TodoList />
-          {/* <ul className="todo">
-            <div className="list-item">
-              <li>Finish To Do App</li>
-            </div>
-            <div className="list-item">
-              <li className="checked">Go to the store</li>
-            </div>
-            <div className="list-item">
-              <li>Do Laundry</li>
-            </div>
-            <div className="list-item">
-              <li>Do Dishes</li>
-            </div>
-          </ul> */}
         </div>
       </div>
     </div>
---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { todoInput: "" };
+  }
+
+  handleChange = (event) => {
+    this.setState({ todoInput: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`A new item was added:  ${this.state.todoInput}`);
+  };
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter Task"
+          value={this.state.todoInput}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input className="btn-new-task" type="submit" value="Add Task" />
+      </form>
+    );
+  }
+}
+
+export default TodoForm;
